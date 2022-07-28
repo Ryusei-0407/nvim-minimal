6de9df2d2a2345782175122764c250188e9dd354
ryu <ryu000407@gmail.com>
2022-07-28 13:58:52 +0900

    update: create init.lua

---
+++ init.lua
@@ -0,0 +1,107 @@
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.cmdheight = 1
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.conceallevel = 0
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+vim.opt.pumheight = 10
+vim.opt.showmode = false
+vim.opt.showtabline = 4
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.timeoutlen = 1000
+vim.opt.undofile = true
+vim.opt.updatetime = 300
+vim.opt.writebackup = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.guifont = "monospace:h10"
+
+vim.opt.shortmess:append("c")
+
+vim.cmd("set whichwrap+=<,>,h,l")
+vim.cmd([[set iskeyword+=-]])
+vim.cmd([[set formatoptions-=cro]])
+
+-- Fast options --
+vim.g.did_install_default_menus = 1
+vim.g.did_install_syntax_menu = 1
+vim.g.did_indent_on = 1
+vim.g.did_load_ftplugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_man = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.skip_loading_mswin = 1
+
+-- Colorscheme --
+vim.cmd([[
+    try
+        colorscheme default
+    catch /^Vim\%((\a\+)\)\=:E185/
+        colorscheme default
+    endtry
+]])
+
+-- Keymap --
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
+
+vim.api.nvim_set_keymap("n", "ss", ":split<CR>", opts)
+vim.api.nvim_set_keymap("n", "sv", ":vsplit<CR>", opts)
+
+-- Treesitter --
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "all",
+	ignore_install = { "phpdoc" },
+	context_commentstring = {
+		enable = true,
+	},
+	highlight = {
+		enable = true,
+		disable = { "lua" },
+	},
+	indent = {
+		enable = true,
+	},
+})
+
+-- Plugins --
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use("lukas-reineke/indent-blankline.nvim")
+end)
